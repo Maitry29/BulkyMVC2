6cf4fd45ac14f2f8961c6caf9ae17ec185b1693c
Maitry <maitrithaker1239@gmail.com>
2024-12-23 19:24:02 +0530

    seed data

---
+++ BulkyWeb2/Controllers/categoryController.cs
@@ -0,0 +1,18 @@
+﻿using BulkyWeb2.Data;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BulkyWeb2.Controllers
+{
+    public class categoryController : Controller
+    {
+        private readonly ApplicationDbContext _db;
+        public categoryController(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ BulkyWeb2/Migrations/20241223135121_Initialmigration.cs
@@ -0,0 +1,55 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
+namespace BulkyWeb2.Migrations
+{
+    /// <inheritdoc />
+    public partial class Initialmigration : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "MyProperty",
+                table: "categories",
+                newName: "Id");
+
+            migrationBuilder.InsertData(
+                table: "categories",
+                columns: new[] { "Id", "DisplayOrder", "Name" },
+                values: new object[,]
+                {
+                    { 1, 1, "Action" },
+                    { 2, 2, "SciFi" },
+                    { 3, 3, "History" }
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "categories",
+                keyColumn: "Id",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "categories",
+                keyColumn: "Id",
+                keyValue: 2);
+
+            migrationBuilder.DeleteData(
+                table: "categories",
+                keyColumn: "Id",
+                keyValue: 3);
+
+            migrationBuilder.RenameColumn(
+                name: "Id",
+                table: "categories",
+                newName: "MyProperty");
+        }
+    }
+}
